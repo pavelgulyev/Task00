cba1cfb5dd1cdba7c859c62ca7e121ca0a53a8d3
Pavel <guliaev.p2014@yandex.ru>
2022-02-03 22:54:55 +0300

    new class

--- src/Model/Procent.java
+++ src/Model/Procent.java
@@ -1,4 +1,13 @@
 package Model;
-
+import java.lang.Math;
 public class Procent {
+    public double countPr(double sum, int pr){
+        return sum*(pr/100.);
+    };
+    public double countSum(double sum, int pr){
+        return sum*(1+pr/100.);
+    };
+    public int countSumTrunc(double sum, int pr){
+        return (int)Math.round(sum*(1+pr/100.));
+    };
 }
--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,14 +1,30 @@
 package sample;
 
+import Model.Procent;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 public class Controller {
     @FXML
+    private Label Label1;
+    @FXML
+    private Label Label2;
+    @FXML
+    private Label Label3;
+    @FXML
+    private TextField Summ;
+    private static final int pr  = 12;
 
-    private Label label1;
-
+    Procent procent = new Procent();
     public void onStart(ActionEvent actionEvent) {
-        label1.setText("Вы нажали на кнопку");
+        Label1.setText(procent.countPr(Double.parseDouble(Summ.getText()),pr)+"");
+    }
+
+    public void onXorosho(ActionEvent actionEvent) {
+        Label2.setText(procent.countSum(Double.parseDouble(Summ.getText()),pr) + "");
+    }
+
+    public void onZamechatelino(ActionEvent actionEvent) {
+        Label3.setText(procent.countSumTrunc(Double.parseDouble(Summ.getText()),pr) + "");
     }
 }
